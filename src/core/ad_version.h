057f0525ea27ecd038bdf6c0b81bb826bbbc747c
pos <pgousdal@users.noreply.github.com>
2026-09-06 22:40:09 +0200

    M0: add native program skeleton

---
+++ src/core/ad_version.h
@@ -0,0 +1,10 @@
+#ifndef AMIDISK_AD_VERSION_H
+#define AMIDISK_AD_VERSION_H
+
+#define AMIDISK_NAME "AmiDisk"
+#define AMIDISK_VERSION "0.0.0-m0"
+#define AMIDISK_TARGET "Motorola 68000 / AmigaOS 2.04+"
+
+const char *ad_version_string(void);
+
+#endif
